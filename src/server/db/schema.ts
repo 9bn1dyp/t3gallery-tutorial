2e34826520ac748f4f5b4850ab4c74cfcafead6e
9bn1dyp <9bn1dyp@gmail.com>
2024-08-16 14:00:07 +0100

    uploadthing, img auth

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -22,14 +22,21 @@ export const images = createTable(
   "image",
   {
     id: serial("id").primaryKey(),
+
     name: varchar("name", { length: 256 }).notNull(),
+
     url: varchar("url", { length: 1024 }).notNull(),
+
+    userId: varchar("userId", { length: 256 }).notNull(),
+
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
+
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
       () => new Date()
     ),
+
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
