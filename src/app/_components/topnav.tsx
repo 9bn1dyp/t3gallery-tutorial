f934c4aa5d7a3b41fd399b6e8dc90b34b2b7776e
9bn1dyp <9bn1dyp@gmail.com>
2024-08-23 13:33:02 +0100

    topnav navigation

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,7 +5,7 @@ export function TopNav() {
 
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
-      <div>Gallery</div>
+      <a href="/">Gallery</a>
   
       <div className="flex flex-row gap-4 items-center">
         <SignedOut>
